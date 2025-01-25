c54be91e3921bf0fe5c103e1090e088bff16d2eb
Urlo30 <91965455+UrloMythus@users.noreply.github.com>
2025-01-25 13:52:28 +0100

    Add files via upload

--- mediaflow_proxy/extractors/factory.py
+++ mediaflow_proxy/extractors/factory.py
@@ -3,12 +3,13 @@ from typing import Dict, Type
 from mediaflow_proxy.extractors.base import BaseExtractor, ExtractorError
 from mediaflow_proxy.extractors.doodstream import DoodStreamExtractor
 from mediaflow_proxy.extractors.livetv import LiveTVExtractor
+from mediaflow_proxy.extractors.maxstream import MaxstreamExtractor
 from mediaflow_proxy.extractors.mixdrop import MixdropExtractor
-from mediaflow_proxy.extractors.uqload import UqloadExtractor
+from mediaflow_proxy.extractors.okru import OkruExtractor
 from mediaflow_proxy.extractors.streamtape import StreamtapeExtractor
 from mediaflow_proxy.extractors.supervideo import SupervideoExtractor
-
-
+from mediaflow_proxy.extractors.uqload import UqloadExtractor
+from mediaflow_proxy.extractors.vixcloud import VixCloudExtractor
 
 
 class ExtractorFactory:
@@ -20,6 +21,9 @@ class ExtractorFactory:
         "Mixdrop": MixdropExtractor,
         "Streamtape": StreamtapeExtractor,
         "Supervideo": SupervideoExtractor,
+        "VixCloud": VixCloudExtractor,
+        "Okru": OkruExtractor,
+        "Maxstream": MaxstreamExtractor,
         "LiveTV": LiveTVExtractor,
     }
 

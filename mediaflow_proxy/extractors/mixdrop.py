8f8c3b195eb60dc558e6542f91fb7cba329786ba
UrloMythus <urlo30@proton.me>
2025-09-01 18:41:27 +0200

    updated to lastest version

--- mediaflow_proxy/extractors/mixdrop.py
+++ mediaflow_proxy/extractors/mixdrop.py
@@ -13,8 +13,9 @@ class MixdropExtractor(BaseExtractor):
             url = url.replace("club", "ps").split("/2")[0]
 
         headers = {"accept-language": "en-US,en;q=0.5"}
+        pattern = r'MDCore.wurl ?= ?"(.*?)"'
 
-        final_url = f"https:{await eval_solver(self, url, headers, r'MDCore.wurl ?= ?"(.*?)"')}"
+        final_url = f"https:{await eval_solver(self, url, headers, pattern)}"
 
         self.base_headers["referer"] = url
         return {
